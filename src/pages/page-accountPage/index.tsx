4143e82e365813697409ea15f2fbc7263614a572
xl2412 <1293358059@qq.com>
2021-06-08 16:56:54 +0800

    pref: clear console

--- src/pages/page-accountPage/index.tsx
+++ src/pages/page-accountPage/index.tsx
@@ -8,38 +8,44 @@ import Transfer from "./Transfer";
 function Account(): React.ReactElement {
   const list = [
     {
-      title: '账户地址',
-      content: <div className='text-blue-light'>f0919523d2b618195e99ADHSLCAHSLCL</div>,
+      title: "账户地址",
+      content: (
+        <div className="text-blue-light">f0919523d2b618195e99ADHSLCAHSLCL</div>
+      ),
     },
     {
-      title: '账户公钥',
-      content: <div className='text-blue-light'>f0919523d2b618195e99ADHSLCAHSLCLf0919523d2b618195e99ADHSLCAHSLCL</div>,
+      title: "账户公钥",
+      content: (
+        <div className="text-blue-light">
+          f0919523d2b618195e99ADHSLCAHSLCLf0919523d2b618195e99ADHSLCAHSLCL
+        </div>
+      ),
     },
     {
-      title: '交易数',
-      content: <div className='text-black-dark'>237</div>,
-    }
-  ]
+      title: "交易数",
+      content: <div className="text-black-dark">237</div>,
+    },
+  ];
   const tabList: TabInfo[] = [
     {
-      title: '资产',
-      content: <Assets />
+      title: "资产",
+      content: <Assets />,
     },
     {
-      title: '转账',
-      content: <Transfer />
-    }
+      title: "转账",
+      content: <Transfer />,
+    },
   ];
 
   const setList = (key: string) => {
-    console.log(key,'key')
+    console.log(key, "key");
   };
   return (
-    <div className=''>
+    <div className="">
       <List list={list} />
       <SwitchTabs
         onTabClick={(key: string) => setList(key)}
-        size='lg'
+        size="lg"
         tabList={tabList}
       />
       <Footer />
